85aad9a81cee5cd2196d00d6da282b554f9fa5d6
singfungcheung <singfung.cheung@gmail.com>
2024-03-13 13:24:59 -0400

    Topic Controller

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,21 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        // Converts to Json automatically and returns as HTTP response.
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "Javascript Description")
+        );
+    }
+}
