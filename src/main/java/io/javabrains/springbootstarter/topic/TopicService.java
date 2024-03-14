9e8c496a62455e1b2797c76eb916a365e0de8aab
singfungcheung <singfung.cheung@gmail.com>
2024-03-13 22:23:16 -0400

    Learned the basics of RestAPI and Boot Spring :)

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -2,17 +2,19 @@ package io.javabrains.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList( // Arrays.asList is immutable.
                     new Topic("spring", "Spring Framework", "Spring Framework Description"),
                     new Topic("java", "Core Java", "Core Java Description"),
                     new Topic("javascript", "JavaScript", "Javascript Description")
-            );
+            )
+    );
 
     public List<Topic> getAllTopics() {
         return this.topics;
@@ -20,5 +22,24 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return this.topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        for (int i = 0; i < topics.size(); i++)
+        {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                break;
+            }
+        }
+    }
+
+    public void deleteTopic(Topic topic, String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
